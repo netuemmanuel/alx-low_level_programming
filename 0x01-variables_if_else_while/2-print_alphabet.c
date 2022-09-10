7b4b165295a0a3f36bda925c0829f623e3a027b3
netuemmanuel <aniemmanuele@gmail.com>
2022-09-09 18:37:16 -0700

    Task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,20 +1,20 @@
-#include <stdio.h>
-/**
- * main - Entry point
+#include <stdio.h>                                                                                                              
+/**                                                                                                                             
+ * main - Entry point                                                                                                           
  *
- * Return: Always 0 (Seccess/correct)
- */
-int main(void)
-
-{
-char alphabet;
-
-for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-{
-putchar(alphabet);
-}
-
-putchar('\n');
-
-return (0);
-
+ * Return: Always 0 (Sucess/correct)
+ */                                                                                                                             
+int main(void)                                                                                                                  
+                                                                                                                                
+{                                                                                                                               
+char alphabet;                                                                                                                  
+                                                                                                                                
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)                                                                               
+{                                                                                                                               
+putchar(alphabet);                                                                                                              
+}                                                                                                                               
+                                                                                                                                
+putchar('\n');                                                                                                                  
+                                                                                                                                
+return (0);                                                                                                                     
+                                                                                                                                
