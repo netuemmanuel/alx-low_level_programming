2da66b57096a5954f09e6487e3b3de53fdd6447c
netuemmanuel <aniemmanuele@gmail.com>
2022-09-09 18:04:33 -0700

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,33 +1,33 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
-
-/**
- * main - Enrty point
- *
- * Return: Always 0 (Seccess/correct)
- */
-
-int main(void)
-{
-int n;
-
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-
-if (n == 0)
-{
-printf("%i is zero\n", n);
-}
-
-else if (n < 0)
-{
-printf("%i is negative\n", n);
-}
-
-else
-{
-printf("%i is positive\n"; n);
-}
-
-return (0);
+#include <stdlib.h>                                                                                                             
+#include <time.h>                                                                                                               
+#include <stdio.h>                                                                                                              
+                                                                                                                                
+/**                                                                                                                             
+ * main - Enrty point                                                                                                           
+ *                                                                                                                              
+ * Return: Always 0 (Seccess/correct)                                                                                           
+ */                                                                                                                             
+                                                                                                                                
+int main(void)                                                                                                                  
+{                                                                                                                               
+int n;                                                                                                                          
+                                                                                                                                
+srand(time(0));                                                                                                                 
+n = rand() - RAND_MAX / 2;                                                                                                      
+                                                                                                                                
+if (n == 0)                                                                                                                     
+{                                                                                                                               
+printf("%i is zero\n", n);                                                                                                      
+}                                                                                                                               
+                                                                                                                                
+else if (n < 0)                                                                                                                 
+{                                                                                                                               
+printf("%i is negative\n", n);                                                                                                  
+}                                                                                                                               
+                                                                                                                                
+else                                                                                                                            
+{                                                                                                                               
+printf("%i is positive\n"; n);                                                                                                  
+}                                                                                                                               
+                                                                                                                                
+return (0);                                                                                                                     
