c19fb2ec71d03e272d218ded542e337532bbb88d
netuemmanuel <aniemmanuele@gmail.com>
2022-09-15 05:54:07 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,6 +1,5 @@
 #include "main.h"
 
-
 /**
  * main - tests function that prints if integer is positive or negative
  * Return: 0
@@ -11,7 +10,7 @@ int main(void)
 int i;
 
 i = 0;
-postive_or_negative(i);
+positive_or_negative(i);
 
 return (0);
 }
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
+#include <stdio.>
 
 void positive_or_negative(int i);
 int largest_number(int, int, int);
