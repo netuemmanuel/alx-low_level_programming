0ff867da459ff3ca156a7c2ed0d79c719587a649
netuemmanuel <aniemmanuele@gmail.com>
2022-09-26 18:19:19 -0700

    4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -4,7 +4,7 @@
  * _strpbrk - searches a string for any of a set of bytes
  * @s: string to check
  * @accept: string to check against
- * 
+ *
  * Return: pointer to byte in s that matches or NULL if no match
  */
 char *_strpbrk(char *s, char *accept)
