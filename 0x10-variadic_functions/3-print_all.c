a35a1237546f2a81457d9487fd66e1a4d6d2a739
netuemmanuel <aniemmanuele@gmail.com>
2022-10-13 17:34:23 -0700

    Task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -24,9 +24,9 @@ void print_all(const char * const format, ...)
 		switch (format[i])
 		{
 			case 'c':
-				printf("%s%c", separator,  va_arg(args, int));
+				printf("%s%c", separator, va_arg(args, int));
 				break;
-			case 'i': 
+			case'i':
 				printf("%s%d", separator, va_arg(args, int));
 				break;
 			case 'f':
