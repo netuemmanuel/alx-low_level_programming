ecfb9ceba544ca536840c50392dfd79d701940db
netuemmanuel <aniemmanuele@gmail.com>
2022-10-20 14:01:40 -0700

    Task 0

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,9 +1,7 @@
-#ifndef _LISTS_
-#define _LISTS_
+#ifndef LISTS_H
+#define LISTS_H
 
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
 /**
  * struct list_s - simply linked list
@@ -26,4 +24,4 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_h *head);
 
-#endif
+#endif /* LISTS_H */
